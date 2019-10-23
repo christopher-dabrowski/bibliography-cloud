d3b98aa01c60565e034f2fca84513f5fbf5fb4ca
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-10-23 11:39:43 +0200

    Wyświetlanie błędów dla poszczególnych pól

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,41 @@
+let tmp; //Variable for testing 
+
 // These are the constraints used to validate the form
 const constraints = {
+    firstname: {
+        // ^ at the start of the message prevents auto insert of field name
+        presence: {message: "^Imię jest wymagane"},
+        format: {
+            // Name must begin with [A-Z] or a Polish character, followed by at least one lowercase [a-z] or a Polish character
+            pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+$/,
+            message: "^Imię musi zaczynać się wielką literą, po które wystąpią małe litery"
+            }
+    },
     email: {
         // Email is required
-        presence: true,
+        presence: {message : "^Email jest wymagany"},
         // and must be an email (duh)
-        email: true
+        // email: true
     }
 };
 
-// TODO: Drugie pole na hasło
-
 $(document).ready(function () {
-    bsCustomFileInput.init();
+    bsCustomFileInput.init(); // Make file input field responsive 
 
     // Hook up the form so we can prevent it from being posted
-    var form = document.getElementById("form");
+    const form = document.getElementById("form");
     form.addEventListener("submit", function (ev) {
-        // ev.preventDefault();
-        // handleFormSubmit(form);
+        ev.preventDefault();
+        handleFormSubmit(form);
     });
-
-    console.log("Test");
 
     // Hook up the inputs to validate on the fly
     var inputs = document.querySelectorAll("input, textarea, select")
     for (var i = 0; i < inputs.length; ++i) {
         inputs.item(i).addEventListener("change", function (ev) {
+            console.log(validate(form, constraints))
             var errors = validate(form, constraints) || {};
-            showErrorsForInput(this, errors[this.name])
+            showErrorsForInput(ev.target, errors[ev.target.name])
         });
     }
 });
@@ -38,11 +46,11 @@ function handleFormSubmit(form, input) {
     // then we update the form to reflect the results
     showErrors(form, errors || {});
     if (!errors) {
-        showSuccess();
+        document.getElementById("form").submit();
     }
 }
 
-// Recusively finds the closest parent that has the specified class
+// Recursively finds the closest parent that has the specified class
 function closestParent(child, className) {
     if (!child || child == document) {
         return null;
@@ -54,14 +62,14 @@ function closestParent(child, className) {
     }
 }
 
+// Clear formGroup
 function resetFormGroup(formGroup) {
-    // Remove the success and error classes
     formGroup.classList.remove("has-error");
     formGroup.classList.remove("has-success");
-    // and remove any old messages
-    $.each(formGroup.querySelectorAll(".help-block.error"), function (el) {
-        el.parentNode.removeChild(el);
-    });
+    
+    for(const message of formGroup.getElementsByClassName("help-block")) {
+        formGroup.removeChild(message);
+    }
 }
 
 // Adds the specified error with the following markup
@@ -74,8 +82,12 @@ function addError(container, message) {
     container.appendChild(block);
 }
 
+// TODO: Get how showErrors() work
 // Updates the inputs with the validation errors
 function showErrors(form, errors) {
+    return; 
+    // TODO: Show errors
+
     // We loop through all the inputs and show the errors for that input
     $.each(form.querySelectorAll("input[name], select[name]"), function (input) {
         // Since the errors can be null if no errors were found we need to handle
@@ -86,22 +98,15 @@ function showErrors(form, errors) {
 
 // Shows the errors for a specific input
 function showErrorsForInput(input, errors) {
-    // This is the root of the input
-    var formGroup = closestParent(input.parentNode, "form-group");
-        // Find where the error messages will be insert into
-        // , messages = formGroup.querySelector(".messages");
-    // First we remove any old messages and resets the classes
+    const formGroup = closestParent(input.parentNode, "form-group");
     resetFormGroup(formGroup);
-    // If we have errors
+    
     if (errors) {
-        // we first mark the group has having errors
         formGroup.classList.add("has-error");
-        // then we append all the errors
-        $.each(errors, function (error) {
+        for (const error of errors) {
             addError(formGroup, error);
-        });
+        }
     } else {
-        // otherwise we simply mark it as success
         formGroup.classList.add("has-success");
     }
 }
