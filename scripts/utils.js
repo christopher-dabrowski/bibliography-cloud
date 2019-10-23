03bf07ac58b45015dffd62661d5550efae1a8a28
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-10-23 20:49:46 +0200

    Podzielenie kodu na pliki

---
+++ scripts/utils.js
@@ -0,0 +1,11 @@
+// Recursively finds the closest parent that has the specified class
+function closestParent(child, className) {
+    if (!child || child == document) {
+        return null;
+    }
+    if (child.classList.contains(className)) {
+        return child;
+    } else {
+        return closestParent(child.parentNode, className);
+    }
+}
