2a77b777915c70dba15e7b610bc3fb446f34eb1f
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-11-14 15:29:01 +0100

    Live reaload podczas produkcji

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 from flask import render_template
+from livereload import Server
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 @app.route('/index')
 @app.route('/signup')
 def sing_up():
     return render_template('signup.html')
+
+if 'DEBUG' in os.environ and os.environ['DEBUG'].lower() == 'true':
+    server = Server(app.wsgi_app)
+    # server.watch
+    server.serve(port=5000)
