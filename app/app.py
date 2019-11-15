6657f2967ffc438a0dba84b3e7e465468ff0d30d
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-11-16 00:25:12 +0100

    Navbar

--- app/app.py
+++ app/app.py
@@ -11,6 +11,10 @@ app.debug = True
 
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
 @app.route('/signup')
 def sing_up():
     return render_template('signup.html')
