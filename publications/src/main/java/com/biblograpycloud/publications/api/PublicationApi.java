65d03e5a9aad839463d4592c04ba48999b19d4be
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-12-16 15:48:26 +0100

    Usunięcie większości starego API

--- publications/src/main/java/com/biblograpycloud/publications/api/PublicationApi.java
+++ publications/src/main/java/com/biblograpycloud/publications/api/PublicationApi.java
@@ -30,7 +30,7 @@ public class PublicationApi {
     public ResponseEntity<?> getAllUserPublication(@PathVariable String user) {
         // TODO: Cleanup link tests
 //        var link = new Link("/place", "next");
-        var link = linkTo(methodOn(PublicationApi.class).getAll()).withRel("me");
+        var link = linkTo(methodOn(PublicationApi.class).getAllUserPublication(user)).withRel("me");
         System.out.println(link);
 
 //        Link selfLink = link.andAffordance(afford(methodOn(PublicationApi.class).deletePublication(user, 1)));
@@ -61,46 +61,6 @@ public class PublicationApi {
         // TODO: Make deleting work
         return ResponseEntity.ok(String.format("Publication number %d deleted", id));
     }
-
-    @GetMapping("/all")
-    public ResponseEntity<Iterable<Publication>> getAll() {
-
-        var result = publicationManager.getAll();
-
-        return ResponseEntity.ok(result);
-
-
-//        var representationModel = new RepresentationModel();
-//        representationModel.add(new Link("/publications", "publication.create"));
-//        representationModel.add(new Link("/publications", "publication.delete"));
-//
-//        return ResponseEntity.ok().body(representationModel);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Publication> getById(@PathVariable long id) {
-        var maybePublication = publicationManager.getById(id);
-
-//        return maybePublication.ifPresentOrElse(p -> ResponseEntity.ok(p), () -> ResponseEntity.notFound().build());
-
-        if (maybePublication.isPresent())
-            return ResponseEntity.ok(maybePublication.get());
-        else
-            return ResponseEntity.notFound().build();
-    }
-
-//    @PostMapping
-//    public ResponseEntity<Publication> addPublication(@RequestParam String owner,
-//                                                      @RequestParam String title,
-//                                                      @RequestParam Integer pageCount,
-//                                                      @RequestParam Integer publicationYear) {
-//
-//        var publication = new Publication(owner, title, pageCount, publicationYear, new ArrayList<>(), new ArrayList<>());
-//        var result = publicationManager.save(publication);
-//
-//        return ResponseEntity.ok(result);
-////        return new ResponseEntity<Publication>(result, HttpStatus.OK);
-//    }
 
     @PostMapping
     public ResponseEntity addPublication(@RequestBody Publication publication) {
