ae8e4533327f4b559527d8cadee587dd2c8c7488
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-04 23:55:08 +0100

    API: Akcja dodania załącznika

--- publications/src/main/java/com/biblograpycloud/publications/api/AttachmentController.java
+++ publications/src/main/java/com/biblograpycloud/publications/api/AttachmentController.java
@@ -1,16 +1,20 @@
 package com.biblograpycloud.publications.api;
 
+import com.biblograpycloud.publications.dao.entity.UserFile;
 import com.biblograpycloud.publications.dto.Translator;
 import com.biblograpycloud.publications.dto.errors.ErrorMessage;
 import com.biblograpycloud.publications.dto.errors.ErrorMessages;
 import com.biblograpycloud.publications.dto.errors.PublicationNotFoundErrorMessage;
 import com.biblograpycloud.publications.managers.PublicationManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -56,5 +60,21 @@ public class AttachmentController {
         publicationManager.save(publication);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/users/{user}/publications/{publicationId}/attachments")
+    ResponseEntity<?> attachFile(@PathVariable String user, @PathVariable Long publicationId,
+                                 @RequestBody UserFile attachment) {
+        var maybePublication = publicationManager.getById(publicationId);
+        if (maybePublication.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new PublicationNotFoundErrorMessage());
+
+        var publication = maybePublication.get();
+        publication.getAttachments().add(attachment);
+        publicationManager.save(publication);
+
+        var link = linkTo(WebMvcLinkBuilder.methodOn(PublicationController.class)
+                .getOnePublication(user, publicationId)).toUri();
+        return ResponseEntity.created(link).build();
     }
 }
