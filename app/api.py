ddf299a07e497bd2cffe01458d6301f42374f16e
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-05 21:22:58 +0100

    Endpoint w Pythonie do pobierania tokenów jwt

--- app/api.py
+++ app/api.py
@@ -1,7 +1,8 @@
 """Flask template for interaction with other services"""
 
-from flask import Blueprint, request
+from flask import Blueprint, request, abort, jsonify
 from decorators import login_required
+from jwt_tokens import create_list_token
 
 api = Blueprint('api', __name__, template_folder='templates')
 
@@ -10,3 +11,18 @@ api = Blueprint('api', __name__, template_folder='templates')
 @login_required
 def get_login(login):
     return login
+
+
+@api.route('jwt/<string:type>')
+@login_required
+def get_token(type, login):
+    """Way for React frontend to get JWT tokens"""
+
+    token = None
+    if (type == 'listFiles'):
+        token = create_list_token(login)
+
+    if token == None:
+        abort(404)
+
+    return jsonify({'token': token})
