cb5fa8c706db906c8987b4d549290d92ab280b43
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-06 18:22:49 +0100

    Stworzenie aplikacji Flutter

---
+++ mobile/mobile_client_flutter/android/app/src/main/kotlin/pl/edu/pw/mobile_client_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package pl.edu.pw.mobile_client_flutter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
