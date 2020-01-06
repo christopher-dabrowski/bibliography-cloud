1afb310b666a40306589d89c2a61bc5381812b81
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-06 19:22:40 +0100

    Validacja loginu i hasła

--- mobile/mobile_client_flutter/lib/LoginScreen.dart
+++ mobile/mobile_client_flutter/lib/LoginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'utils.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -9,9 +10,34 @@ class _LoginScreenState extends State<LoginScreen> {
   final Map<String, String> credentials = {'jan':'AAA', 'zupan':'gros', 'Atrox': 'password'};
   final TextStyle textStyle = TextStyle(fontSize: 20.0);
 
+  final loginController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void handleSubmit() {
+    var login = loginController.text;
+    if (!credentials.containsKey(login)) {
+      Utils.displayAlert(context, 'Niepoprawne dane', 'Błędny login');
+      return;
+    }
+    if (credentials[login] != passwordController.text) {
+      Utils.displayAlert(context, 'Niepoprawne dane', 'Błędne hasło');
+      return;
+    }
+
+    
+  }
+
+  @override
+  void dispose() {
+    loginController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final loginField = TextField(
+      controller: loginController,
       obscureText: false,
       style: textStyle,
       decoration: InputDecoration(
@@ -22,11 +48,12 @@ class _LoginScreenState extends State<LoginScreen> {
     );
 
     final passwordField = TextField(
+      controller: passwordController,
       obscureText: true,
       style: textStyle,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Password",
+          hintText: "Hasło",
           border:
           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
@@ -38,8 +65,8 @@ class _LoginScreenState extends State<LoginScreen> {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
-        child: Text("Login",
+        onPressed: handleSubmit,
+        child: Text("Zaloguj",
             textAlign: TextAlign.center,
             style: textStyle.copyWith(
                 color: Colors.white, fontWeight: FontWeight.bold)),
@@ -65,7 +92,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
-                          Text('Login page', style: textStyle,)
+                          Text('Bibliograpy Cloud App', style: textStyle,)
                         ],
                       ),
                     )
