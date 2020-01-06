b3a4f550a7eef08347ebc4c904f1a4451ac75ee9
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-06 19:22:22 +0100

    Funkcja do powiadomień

---
+++ mobile/mobile_client_flutter/lib/utils.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class Utils {
+  static void displayAlert(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text(title),
+          content: new Text(message),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
