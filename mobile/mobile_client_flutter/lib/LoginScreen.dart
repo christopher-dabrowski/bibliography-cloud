3d1531ae6f0856156df9ffbe405c7b5d04d71711
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-06 18:53:48 +0100

    Wygląd ekranu logowania

---
+++ mobile/mobile_client_flutter/lib/LoginScreen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final Map<String, String> credentials = {'jan':'AAA', 'zupan':'gros', 'Atrox': 'password'};
+  final TextStyle textStyle = TextStyle(fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context) {
+    final loginField = TextField(
+      obscureText: false,
+      style: textStyle,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Login",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final passwordField = TextField(
+      obscureText: true,
+      style: textStyle,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: textStyle.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 50.0,
+                  width: MediaQuery.of(context).size.width,
+                  child:
+                    Material(
+                      elevation: 5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Login page', style: textStyle,)
+                        ],
+                      ),
+                    )
+                ),
+                SizedBox(height: 45.0),
+                loginField,
+                SizedBox(height: 25.0),
+                passwordField,
+                SizedBox(
+                  height: 35.0,
+                ),
+                loginButon,
+                SizedBox(
+                  height: 15.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
