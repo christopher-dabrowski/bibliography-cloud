c09840ad3d89f7bec153c13e9bca73ea642622c2
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-18 16:39:09 +0100

    Przekazywanie UserManager przez Config

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, ValidationError, FileField
 from wtforms.validators import DataRequired, Email
-from users import UserManager
 from config import Config
 
 
